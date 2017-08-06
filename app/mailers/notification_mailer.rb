dfdd2443ae8aad918e3481700bd7ed1296465f38
Alexandre Bini <alexandrebini@gmail.com>
2017-08-06 13:59:12 -0300

    product update mail template

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,18 @@ class NotificationMailer < ApplicationMailer
   def product_update(product_id, user_id)
     @user = User.find_by(id: user_id)
     @product = Product.find_by(id: product_id)
-    return if @product.blank? || @user.blank?
+    @changeset = @product.versions.last.changeset
+
+    return if @product.blank? || @user.blank? || !changed?(@product)
+
     mail(to: @user.email, subject: "[Product Update] #{@product.title}")
+  end
+
+  private
+
+  def changed?(product)
+    product.versions.last.changeset.any? do |key, _value|
+      Product::NOTIFICABLE_ATTRIBUTES.include?(key.to_sym)
+    end
   end
 end
