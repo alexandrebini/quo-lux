fee81b954aa8112f2e3b6bbc9c536a30de035cd5
Alexandre Bini <alexandrebini@gmail.com>
2017-08-07 00:08:52 -0300

    Inventory Tracker

--- lib/amazon/inventory.rb
+++ lib/amazon/inventory.rb
@@ -1,7 +1,18 @@
 module Amazon
   class Inventory < Base
+    attr_accessor :cart_page
+
     def value
       # TODO
+    end
+
+    def add_to_cart!
+    end
+
+    private
+
+    def add_to_chart_form
+      page.form_with(id: 'addToCart')
     end
   end
 end
